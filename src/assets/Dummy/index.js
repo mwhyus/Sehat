b0fc8ec7a111a3247cc841a61be37e9df8933ffc
mwhyus <mwahyus17@gmail.com>
2021-04-02 16:26:29 +0700

    init

---
+++ src/assets/Dummy/index.js
@@ -0,0 +1,26 @@
+import DummyUser from './UserPic.png'
+import DummyNews1 from './News1.png'
+import DummyNews2 from './News2.png'
+import DummyNews3 from './News3.png'
+import DummyDoc1 from './Doc1.png'
+import DummyDoc2 from './Doc2.png'
+import DummyDoc3 from './Doc3.png'
+import DummyDoc4 from './Doc4.png'
+import DummyHospital1 from './Hospital1.png'
+import DummyHospital2 from './Hospital2.png'
+import DummyHospital3 from './Hospital3.png'
+
+export {
+    DummyUser,
+    DummyNews1,
+    DummyNews2,
+    DummyNews3,
+    DummyDoc1,
+    DummyDoc2,
+    DummyDoc3,
+    DummyDoc4,
+    DummyHospital1,
+    DummyHospital2,
+    DummyHospital3,
+    
+}
--- src/assets/Illustration/index.js
+++ src/assets/Illustration/index.js
@@ -1,3 +1,27 @@
-import ILSehat from './SIRIUS.svg'
+import ILFirst from './SplashScreen.svg'
+import ILGetStarted from './get-started.png'
+import ILText from './text.png'
+import ILNullPhoto from './null_user.png'
+import ILDocGen from './doc-gen.svg'
+import ILDocPed from './doc-ped.svg'
+import ILDocPsy from './doc-psy.svg'
+import ILDocVet from './doc-vet.svg'
+import ILDocDen from './doc-den.svg'
+import ILMedicine from './medicine.svg'
+import ILCover from './cover.png'
+import ILSehat from './Sehat.png'
 
-export {ILSehat}
+export {
+    ILFirst,
+    ILGetStarted,
+    ILText,
+    ILNullPhoto,
+    ILDocGen,
+    ILDocPed,
+    ILDocPsy,
+    ILDocVet,
+    ILDocDen,
+    ILMedicine,
+    ILCover,
+    ILSehat
+}
